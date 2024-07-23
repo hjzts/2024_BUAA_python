bf82c609f0e35cec67dc1e0731efa4737c1063ac
ap0stader <ap0stader@outlook.com>
2024-07-23 17:22:23 +0800

    添加做题模块

---
+++ src/components/ExerciseFinisher.vue
@@ -0,0 +1,10 @@
+<template>
+    <!-- 做题的 -->
+    <v-card>
+
+    </v-card>
+</template>
+
+<script lang="ts" setup name="ExerciseFinisher"></script>
+
+<style scoped></style>
---
+++ src/components/ExerciseUpdater.vue
@@ -0,0 +1,13 @@
+<template>
+    <!-- 改题的 -->
+    <v-card>
+
+    </v-card>
+    <v-dialog >
+
+    </v-dialog>
+</template>
+
+<script lang="ts" setup name="ExerciseUpdater"></script>
+
+<style scoped></style>
