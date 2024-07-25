9a5c139850eb8a34de0f4c4c662f9fff89859a5e
hjzts <119279415+hjzts@users.noreply.github.com>
2024-07-25 10:16:13 +0800

    test:/测试无框表格的实现

--- src/utils/exercise.ts
+++ src/utils/exercise.ts
@@ -1,4 +1,4 @@
-import type { NewExercise } from "@/types"
+import type { NewExercise, NewExerciseItem } from "@/types"
 
 export function getNewExerciseModel(): NewExercise {
     return {
@@ -8,5 +8,34 @@ export function getNewExerciseModel(): NewExercise {
         option: [],
         answer: [],
         tag: [],
+    }
+}
+
+export function getDefaultExerciseModel(type:0 | 1 | 2 | 10 ): NewExerciseItem {
+    let exercise:NewExercise={
+        type: type,
+        title:  type===0 ? "python判断题" : 
+                type===1 ? "python单选题" :
+                type===2 ? "python多选题" :
+                "python填空题",
+        content: type===0 ? "请判断以下代码的输出结果" :
+                    type===1 ? "以下哪个选项是正确的" :
+                    type===2 ? "以下哪些选项是正确的" :
+                    "请填写下面的代码",
+        option: type===0 ? ["A", "B"] :
+                type===1 ? ["A", "B", "C", "D"] :
+                type===2 ? ["A", "B", "C", "D"] :
+                ["____"],
+        answer: type===0 ? ["A"] :
+                type===1 ? ["A"] :
+                type===2 ? ["A", "B"] :
+                ["__填空题答案__"],
+        tag: [0]
+    }
+    return {
+        exerciseid: Math.floor(Math.random()*100),
+        exercise: exercise,
+        is_upload_success:true,
+        last_upload_time: new Date().toLocaleString()
     }
 }
--- src/views/Home/AllExercise.vue
+++ src/views/Home/AllExercise.vue
@@ -1,5 +1,70 @@
-<template>所有题目</template>
+<template>
+    <v-data-table :headers="headers" :items="items" class="elevation-1">
+        <template v-slot:item.passed="{ item }">
+            <v-checkbox v-model="item.passed" hide-details color="success" readonly></v-checkbox>
+        </template>
+        <template v-slot:item.action="{ item }">
+            <v-btn icon @click="seeDetails(item)">
+                <v-icon>mdi-file-document</v-icon>
+            </v-btn>
+        </template>
 
-<script lang="ts" setup name="AllExercise"></script>
+        <v-dialog max-width="500">
+            <template v-slot:activator="{ props: activatorProps }">
+                <v-btn v-bind="activatorProps" color="surface-variant" text="Open Dialog" variant="flat"></v-btn>
+            </template>
+
+            <template v-slot:default="{ isActive }">
+                <v-card title="Dialog">
+                    <v-card-text>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                        et dolore magna aliqua.
+                    </v-card-text>
+
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn text="Close Dialog" @click="isActive.value = false"></v-btn>
+                    </v-card-actions>
+                </v-card>
+            </template>
+        </v-dialog>
+
+    </v-data-table>
+</template>
+
+<script lang="ts" setup name="AllExercise">
+    import { getDefaultExerciseModel } from '@/utils/exercise';
+    import { ref } from 'vue'
+    let dialogDelete = ref(false)
+    let headers = ref([
+        { text: '测试点 ID', value: 'id' },
+        { text: '名称', value: 'name' },
+        { text: '已通过', value: 'passed' },
+        { text: '判定信息', value: 'status' },
+        { text: '分数', value: 'score' },
+        { text: '操作', value: 'action', sortable: false },
+    ])
+    let items = ref([
+        { id: 3971, name: '2024_unit1_hw1_strong_1', passed: false, status: 'ACCEPTED', score: 100 },
+        { id: 3972, name: '2024_unit1_hw1_strong_2', passed: true, status: 'ACCEPTED', score: 100 },
+        { id: 3973, name: '2024_unit1_hw1_strong_3', passed: true, status: 'ACCEPTED', score: 100 },
+        { id: 3974, name: '2024_unit1_hw1_strong_4', passed: true, status: 'ACCEPTED', score: 100 },
+        { id: 3975, name: '2024_unit1_hw1_strong_5', passed: true, status: 'ACCEPTED', score: 100 },
+        { id: 3976, name: '2024_unit1_hw1_strong_6', passed: false, status: 'ACCEPTED', score: 100 },
+        { id: 3977, name: '2024_unit1_hw1_strong_7', passed: true, status: 'ACCEPTED', score: 100 },
+        { id: 3978, name: '2024_unit1_hw1_strong_8', passed: true, status: 'ACCEPTED', score: 100 },
+        { id: 3979, name: '2024_unit1_hw1_strong_9', passed: true, status: 'ACCEPTED', score: 100 },
+        { id: 3980, name: '2024_unit1_hw1_strong_10', passed: true, status: 'ACCEPTED', score: 100 },
+    ])
+
+    function seeDetails(item: any) {
+        console.log(item)
+        dialogDelete.value = true
+    }
+
+    function closeDelete() {
+        dialogDelete.value = false
+    }
+</script>
 
 <style scoped></style>
