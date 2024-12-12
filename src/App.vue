fca81e02e812cbf30cc896802b8a6b16ef901cbf
hjzts <119279415+hjzts@users.noreply.github.com>
2024-12-12 23:04:51 +0800

    test🌱:/测试水平居中功能

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
-      <div class="centered-text">
-    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-  </div>
-
+    <v-app theme="light">
+        <RouterView />
+    </v-app>
+    <ErrorSnackbars />
+    <SuccessSnackbars />
 </template>
 
 <script setup lang="ts" name="App">
@@ -11,11 +12,4 @@
     import SuccessSnackbars from "./components/SuccessSnackbars.vue"
 </script>
 
-<style scoped>
-.centered-text {
-  display: flex; /* 使容器成为 Flexbox */
-  justify-content: center; /* 水平居中 */
-  height: 100vh; /* 使容器高度为视口高度 */
-  align-items: center;    /* 垂直居中 */
-}
-</style>
+<style scoped></style>
